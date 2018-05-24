11942559f6762bef23525cee98ba98d11ed18162
xiaolanlaia <946372869@qq.com>
2018-05-24 13:24:58 +0800

    Completed.

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/12/15 0015.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -0,0 +1,99 @@
+package com.coolweather.android.util;
+
+import android.text.TextUtils;
+
+import com.coolweather.android.db.City;
+import com.coolweather.android.db.County;
+import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Administrator on 2017/12/15 0015.
+ */
+
+public class Utility {
+    /**
+     * Parsing and processing provincial data returned by the server;
+     */
+    public static boolean handleProvinceResponse(String response){
+        if (!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0;i <allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    /**
+     * Parsing and processing municipal data returned by server;
+     */
+    public static boolean handleCityResponse(String response,int provinceId){
+        if (!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0;i <allCities.length();i++){
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    /**
+     * Parsing and processing county-level data returned by server;
+     */
+    public static boolean handleCountyResponse(String response,int cityId){
+        if (!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCounties = new JSONArray(response);
+                for (int i = 0;i <allCounties.length();i++){
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    /**
+     * Parsing the returned JSON data into a weather entity class;
+     */
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
